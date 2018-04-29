99b1fd486a90a0af55dfbfffa8efc962a7d4413c
Tim Tucker <tntucker0015@outlook.com>
2018-04-29 10:22:50 -0400

    added twitter, spotify, and omdb api calls

--- keys.js
+++ keys.js
@@ -1,10 +1,8 @@
-console.log('this is loaded');
-
 exports.twitter = {
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  acees_token_secret: process.envTWITTER_ACCESS_TOKEN_SECRET,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 };
 
 exports.spotify = {
--- liri.js
+++ liri.js
@@ -1,31 +1,79 @@
 require("dotenv").config();
 
 
-var client = new Twitter(keys.twitter);
+const input = process.argv;
+const key = require('./keys.js');
+const twitKeys = key.twitter;
+const omdb = key.omdb.id;
+const Twitter = require("twitter");
+const twitterSearch = "input search";
+const client = new Twitter(key.twitter);
+const params = {
+  q: twitterSearch,
+  screen_name: 'uncpappabear',
+  count: 10,
+  exclude_replies: true,
+  trim_user: true,
+};
 
-// twitter call api
-var Twitter = require('twitter'); 
-var params = {screen_name: 'nodejs'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    console.log(tweets);
+// Twitter search
+// client.get('search/tweets', params, searchTweets);
+// function searchTweets(error, data, response) {
+//   for (var i = 0; i < data.statuses.length; i++) {
+//     console.log("========================================");  
+//     console.log(data.statuses[i].created_at);
+//     console.log(data.statuses[i].text);
+//   }
+// };
+
+// My Tweets
+client.get('statuses/user_timeline', params, userTweets);
+function userTweets(error, data, response) {
+  for (var i = 0; i < data.length; i++) {
+    console.log("========================================"); 
+    console.log(params.screen_name);
+    console.log(data[i].created_at);
+    console.log(data[i].text);
   }
-});
+};
 
-// Node-spotify-api
-var Spotify = require('node-spotify-api');
-var spotify = new Spotify(keys.spotify);  
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
-console.log(data); 
-});
+// } else if (input [2] === "spotify-this-song") {
+// // Node-spotify-api
+// var Spotify = require('node-spotify-api');
+// var spotify = new Spotify(keys.spotify);  
+// spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+//   if (err) {
+//     return console.log('Error occurred: ' + err);
+//   }
+// console.log(data); 
+// });
 
-// omdb api
-var omdb = new omdb(keys.omdb)
-var movie;
-var request = require('request');
-request('http://www.omdbapi.com/?apikey=544c20f9?t=bull+durham', function (response) {
-  console.log(response);
-});
+// // omdb api
+// } else if (input[2] === "movie-this") {
+  // var request = require("request");
+  // var movieArgs = input[3];
+  // var movieName = '';
+  // for (var i = 2; i < movieArgs.length; i++) {
+  //   if (i > 2 && i < movieArgs.length) {
+  //     movieName = movieName + "+" + movieArgs[i];
+  //   } else {
+  //     movieName += movieArgs[i];
+  //   }
+  // }
+  
+  // request("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=" + omdb, function (error, response, body) {
+  //     if (!error && response.statusCode === 200) {
+  //       console.log("Title: " + JSON.parse(body).Title);
+  //       console.log("Release Year: " + JSON.parse(body).Year);
+  //       console.log("IMBD Rating: " + JSON.parse(body).imdbRating);
+  //       console.log("Rotten Tomatoes: " + JSON.parse(body).Ratings[1].Value);
+  //       console.log("Country: " + JSON.parse(body).Country);
+  //       console.log("Language: " + JSON.parse(body).Language);
+  //       console.log("Plot: " + JSON.parse(body).Plot);
+  //       console.log("Actors: " + JSON.parse(body).Actors);
+  //     }
+  //   });
+  // } else if (input[2] === "do-what-it-says") {
+
+  // };
+
