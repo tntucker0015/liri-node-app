2246b8ccb1ed4fdc214c7e7ef7757f69fc5158b1
Tim Tucker <tntucker0015@outlook.com>
2018-05-01 18:17:49 -0400

    final changes before submitting homework

--- liri.js
+++ liri.js
@@ -8,6 +8,7 @@ const twitKeys = key.twitter;
 const spotKeys = key.spotify
 const omdb = key.omdb.id;
 const Twitter = require("twitter");
+const doWhatItSays = require('./random.txt');
 const client = new Twitter(key.twitter);
 const params = {
   q: userValue,
@@ -16,94 +17,98 @@ const params = {
   exclude_replies: true,
   trim_user: true,
 };
-
 switch (input) {
   case 'search-tweets':
-// Twitter search
-// +++++++++++++++++++++++++++++++++++++++++++++++
+    // // Twitter search
+    // // +++++++++++++++++++++++++++++++++++++++++++++++
+    client.get('search/tweets', params, searchTweets);
 
-client.get('search/tweets', params, searchTweets);
-function searchTweets(error, data, response) {
-  for (var i = 0; i < data.statuses.length; i++) {
-    console.log("========================================");  
-    console.log(data.statuses[i].created_at);
-    console.log(data.statuses[i].text);
-  }
-};
-  break;
+    function searchTweets(error, data, response) {
+      for (var i = 0; i < data.statuses.length; i++) {
+        console.log("========================================");
+        console.log(data.statuses[i].created_at);
+        console.log(data.statuses[i].text);
+      }
+    };
+    break;
   case 'my-tweets':
-// My Tweets
-client.get('statuses/user_timeline', params, userTweets);
-function userTweets(error, data, response) {
-  for (var i = 0; i < data.length; i++) {
-    console.log("========================================"); 
-    console.log(params.screen_name);
-    console.log(data[i].created_at);
-    console.log(data[i].text);
-  }
-};
-  break;
-case 'spotify-this-song':
-// // Node-spotify-api
-// +++++++++++++++++++++++++++++++++++++++++++++++
-// var info = response.tracks.items
-var searchItem = input[3];
-var Spotify = require('node-spotify-api');
-var spotify = new Spotify(spotKeys);
-var album = '';
-spotify.search({
-  type: 'track',
-  query: userValue,
-}, function (err, response) {
-  album = response.tracks.items[0].album;
-  track = response.tracks.items[0].name;
-  artist = album.artists[0].name;
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
- 
-  console.log("========================================");
-  console.log(track);
-  console.log(artist);
-  console.log(album.name);
-  console.log(album.external_urls);
-  console.log("========================================");
-  
-});
-break;
+    // My Tweets
+    client.get('statuses/user_timeline', params, userTweets);
 
-case 'movie-this':
-// // omdb api
-// //+++++++++++++++++++++++++++++++++++++++++++++++
-var request = require("request");
-var movieArgs = input[3];
-var movieName = '';
-for (var i = 2; i < movieArgs.length; i++) {
-  if (i > 2 && i < movieArgs.length) {
-    movieName = movieName + "+" + movieArgs[i];
-  } else {
-    movieName += movieArgs[i];
-  }
-}
+    function userTweets(error, data, response) {
+      for (var i = 0; i < data.length; i++) {
+        console.log("========================================");
+        console.log(params.screen_name);
+        console.log(data[i].created_at);
+        console.log(data[i].text);
+      }
+    };
+    break;
+  case 'spotify-this-song':
+    // Node-spotify-api
+    // +++++++++++++++++++++++++++++++++++++++++++++++
+    var Spotify = require('node-spotify-api');
+    var spotify = new Spotify(spotKeys);
+    spotify.search({
+      type: 'track',
+      query: userValue
+    }, function (err, data) {
+      if (err) {
+        return console.log('Error occurred: ' + err);
+      }
+      album = data.tracks.items[0].album;
+      track = data.tracks.items[0].name;
+      artist = album.artists[0].name;
+      console.log("========================================");
+      console.log(track);
+      console.log(artist);
+      console.log(album.name);
+      console.log(album.external_urls);
+      console.log("========================================");
 
-request("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=" + omdb, function (error, response, body) {
-    if (!error && response.statusCode === 200) {
-      // console.log(response);
-      console.log("Title: " + JSON.parse(body).Title);
-      // console.log("Release Year: " + JSON.parse(body).Year);
-      // console.log("IMBD Rating: " + JSON.parse(body).imdbRating);
-      // console.log("Rotten Tomatoes: " + JSON.parse(body).Ratings[1].Value);
-      // console.log("Country: " + JSON.parse(body).Country);
-      // console.log("Language: " + JSON.parse(body).Language);
-      // console.log("Plot: " + JSON.parse(body).Plot);
-      // console.log("Actors: " + JSON.parse(body).Actors);
-    }
-  });
-break;
-case 'do-what-it-says':
-//"do-what-it-says") 
+    });
+    break;
+  case 'movie-this':
+    // // // omdb api
+    // // //+++++++++++++++++++++++++++++++++++++++++++++++
+    var request = require("request");
+    var movieName = userValue;
+    request("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=" + omdb, function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        console.log("Title: " + JSON.parse(body).Title);
+        console.log("Release Year: " + JSON.parse(body).Year);
+        console.log("IMBD Rating: " + JSON.parse(body).imdbRating);
+        console.log("Rotten Tomatoes: " + JSON.parse(body).Ratings[1].Value);
+        console.log("Country: " + JSON.parse(body).Country);
+        console.log("Language: " + JSON.parse(body).Language);
+        console.log("Plot: " + JSON.parse(body).Plot);
+        console.log("Actors: " + JSON.parse(body).Actors);
+      }
+    });
+    break;
+  case 'do-what-it-says':
+    if (userValue == '') {
+      const Spotify = require('node-spotify-api');
+      const spotify = new Spotify(spotKeys);
+      spotify.search({
+        type: 'track',
+        query: doWhatItSays,
+      }, function (err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+        album = data.tracks.items[0].album;
+        track = data.tracks.items[0].name;
+        artist = album.artists[0].name;
+        console.log("========================================");
+        console.log(track);
+        console.log(artist);
+        console.log(album.name);
+        console.log(album.external_urls);
+        console.log("========================================");
 
-// };
-break;
-default:
+      });
+    };
+    break;
+  default:
 }
